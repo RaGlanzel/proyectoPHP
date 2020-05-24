b3a546d59beb787e7c65688a1849a18188529599
root <root@RaGlanzel-home>
2020-05-24 00:51:07 -0300

    login completo y categorias sincronizadas con la base de datos

--- includes/login.php
+++ includes/login.php
@@ -4,42 +4,33 @@ require_once 'conexion.php';
 
 
 //recoger datos del formulario
-if(isset($_POST)){
+if (isset($_POST)) {
+    //borrar sesion antigua
+    if (isset($_SESSION['error_login'])) {
+                session_unset($_SESSION['error_login']);
+            }
+       //recoger datos usuario     
     $email = trim($_POST['email']);
     $password = $_POST['password'];
 //consulta para comprobar la credenciales del usuario
-    $sql = "SELECT *  FROM usuarios WHERE email = '$email';";
+    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
     $login = mysqli_query($db, $sql);
 
-    if($login && mysqli_num_rows($login) == 1){
+    if ($login && mysqli_num_rows($login) == 1) {
         $usuario = mysqli_fetch_assoc($login);
 
         //comprobar la contraseña / cifrar
         $verify = password_verify($password, $usuario['password']);
-    if($verify){
+        if ($verify) {
 //utilizar una sesion para guardar  los datos del usuario logueado
-        $_SESSION['usuario'] = $usuario;
-        if(isset($_SESSION['error_login'])){
-            session_unset($_SESSION['error_login']);
+            $_SESSION['usuario'] = $usuario;
+            
+        } else {
+            //si algo falla enviar una sesion con el fallo
+            $_SESSION['error_login'] = "Nombre de usuario o contraseña incorrecta";
         }
-    }else{
-        //si algo falla enviar una sesion con el fallo
+    } else {
         $_SESSION['error_login'] = "Nombre de usuario o contraseña incorrecta";
     }
-
-    }else{
-        $_SESSION['error_login'] = "Nombre de usuario o contraseña incorrecta"; 
-    }
-
-
 }
 header('Location: index.php');
-
-
-
-
-//utlizar una sesion para guardar los datos del usuario logueado
-
-//si algo falla enviar una sesion con el fallo
-
-//redirigir al index.php
