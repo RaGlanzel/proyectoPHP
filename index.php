d2c4d8f8448b7c143a36692871021cfb1901ff4f
root <root@RaGlanzel-home>
2020-05-24 00:46:42 -0300

    login completo y categorias sincronizadas con la base de datos

--- index.php
+++ index.php
@@ -3,26 +3,22 @@
 
 <div class="principal">
     <h1>Ultimas entradas</h1>
+    <?php $entradas = conseguirUltimasEntradas($db);
+    if(!empty($entradas)):
+        while ($entrada = mysqli_fetch_assoc($entradas)):
+      ?>
     <a href="">
         <article class="entradas">
-            <h2>Titulo de mi entrada</h2>
-            <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis </p>
+            <h2><?=$entrada['titulo']?></h2>
+            <span class='fecha'><?=$entrada['Categoria'].' | '.$entrada['fecha']?></span>
+            <p><?= substr($entrada['descripcion'], 0, 180)."..."?></p>
         </article>
     </a>
-    <a href="">
-        <article class="entradas">
-            <h2>Titulo de mi entrada</h2>
-            <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis </p>
-        </article>
-    </a>
-    <a href="">
-        <article class="entradas">
-            <h2>Titulo de mi entrada</h2>
-            <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis </p>
-        </article>
-    </a>
-
-
+    <?php
+        endwhile;
+    endif;
+    ?>
+    
     <div id="ver-todas">
         <a href="">Ver todas la entradas</a>
     </div>
