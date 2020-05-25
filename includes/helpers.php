7cf48f4b8fb4d6b53630895563c65c272910d501
RaGlanzel <raglanzel.rag@gmail.com>
2020-05-25 20:35:29 -0300

    actualización funciones

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,49 +1,88 @@
 <?php
 
-function mostrarError($errores, $campo){
+function mostrarError($errores, $campo) {
     $alert = '';
-    if(isset($errores[$campo]) && !empty($campo)){
-        $alert = "<div class='alerta alerta-error'>".$errores[$campo].'</div>';
+    if (isset($errores[$campo]) && !empty($campo)) {
+        $alert = "<div class='alerta alerta-error'>" . $errores[$campo] . '</div>';
     }
     return $alert;
 }
-function borrarErrores(){
+
+function borrarErrores() {
     $borrado = 0;
-    if(isset($_SESSION['errores'])){
-    $_SESSION['errores'] = null;
-    $borrado = true;
+    if (isset($_SESSION['errores'])) {
+        $_SESSION['errores'] = null;
+        $borrado = true;
     }
-    if(isset($_SESSION['errores_entradas'])){
-    $_SESSION['errores_entradas'] = null;
-    
+    if (isset($_SESSION['errores_entradas'])) {
+        $_SESSION['errores_entradas'] = null;
+        $borrado = true;
     }
-if(isset($_SESSION['completado'])){
-    $_SESSION['completado'] = null;
-    $borrado = true;
-    
-    return $borrado;
+    if (isset($_SESSION['completado'])) {
+        $_SESSION['completado'] = null;
+        $borrado = true;
+
+        return $borrado;
     }
 }
-function conseguirCategorias($conexion){
+
+function conseguirCategorias($conexion) {
     $sql = "SELECT * FROM categorias ORDER BY id ASC;";
     $categorias = mysqli_query($conexion, $sql);
-    
+
     $result = array();
-    if($categorias && mysqli_num_rows($categorias)){
+    if ($categorias && mysqli_num_rows($categorias)) {
         $result = $categorias;
-        
     }
     return $result;
 }
-function conseguirUltimasEntradas($conexion){
-    $sql = "SELECT e.*, c.nombre AS 'Categoria' FROM entradas e ".
-            "INNER JOIN categorias c ON e.categoria_id = c.id ".
-            "ORDER BY e.id DESC LIMIT 4;";
+
+function conseguirCategoria($conexion, $id) {
+    $sql = "SELECT * FROM categorias WHERE id = '$id';";
+    $categorias = mysqli_query($conexion, $sql);
+
+    $result = array();
+    if ($categorias && mysqli_num_rows($categorias)) {
+        $result = mysqli_fetch_assoc($categorias);
+    }
+    return $result;
+}
+
+function conseguirEntradas($conexion, $limit = null, $categoria = null) {
+    $sql = "SELECT e.*, c.nombre AS 'Categoria' FROM entradas e " .
+            "INNER JOIN categorias c ON e.categoria_id = c.id ";
+
+    if (!empty($categoria)) {
+        $sql .= "WHERE e.categoria_id = '$categoria' ";
+    }
+
+    $sql .= "ORDER BY e.id DESC ";
+
+
+    if ($limit) {
+        $sql .= "LIMIT 4";
+    }
+
     $entradas = mysqli_query($conexion, $sql);
-    
+
     $resultado = array();
-    if($entradas && mysqli_num_rows($entradas) >= 1){
+    if ($entradas && mysqli_num_rows($entradas) >= 1) {
         $resultado = $entradas;
     }
     return $entradas;
+}
+
+function conseguirEntrada($conexion, $id) {
+    $sql = "SELECT e.*, c.nombre AS 'categoria', CONCAT(u.nombre, ' ', u.apellidos) AS usuario " .
+            "FROM entradas e " .
+            "INNER JOIN categorias c ON e.categoria_id = c.id " .
+            "INNER JOIN usuarios u ON e.usuario_id = u.id " .
+            "WHERE e.id = $id;";
+    $entrada = mysqli_query($conexion, $sql);
+
+    $resultado = array();
+    if ($entrada && mysqli_num_rows($entrada) >= 1) {
+        $resultado = mysqli_fetch_assoc($entrada);
+    }
+    return $resultado;
 }
